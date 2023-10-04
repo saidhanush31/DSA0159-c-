ee291c0fc02c29a739da4b92fc667b2010f264e4
saidhanush31 <113282925+saidhanush31@users.noreply.github.com>
2023-10-04 07:02:07 +0530

    output

---
+++ Ass1-Q8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float income; 
+cout << "Enter your monthly income: "; 
+cin >> income; 
+if (income < 0.0) 
+cout << "You are going farther into debt every month." << endl; 
+else if (income >= 0.0 && income < 1200.00) 
+cout << "You are living below the poverty line." << endl; 
+else if (income >= 1200.00 && income < 2500.00) 
+cout << "You are living in moderate comfort." << endl; 
+else if (income >= 2500.00) 
+cout << "You are well off." << endl;
+return 0;
+}
